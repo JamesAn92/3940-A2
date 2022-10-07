7ac2964fee8319a1f7c774b560fc984605c64dfc
Tissure <kyle.ng7512@gmail.com>
2022-10-06 19:50:40 -0700

    Add html to uploadservlet

--- servlet/UploadServlet.java
+++ servlet/UploadServlet.java
@@ -12,7 +12,26 @@ public class UploadServlet implements HttpServlet {
         try {
             // TODO Auto-generated method stub
             System.out.println("Upload doGET");
-            response.getOutputStream().write("cum".getBytes());
+            ByteArrayOutputStream writer = response.getOutputStream();
+            writer.write("<!DOCTYPE html>\r\n".getBytes());
+            writer.write("<html>\r\n".getBytes());
+            writer.write("    <head>\r\n".getBytes());
+            writer.write("        <title>File Upload Form</title>\r\n".getBytes());
+            writer.write("    </head>\r\n".getBytes());
+            writer.write("    <body>\r\n".getBytes());
+            writer.write("<h1>Upload file</h1>\r\n".getBytes());
+            writer.write("<form method=\"POST\" action=\"upload\" ".getBytes());
+            writer.write("enctype=\"multipart/form-data\">\r\n".getBytes());
+            writer.write("<input type=\"file\" name=\"fileName\"/><br/><br/>\r\n".getBytes());
+            writer.write("Caption: <input type=\"text\" name=\"caption\"<br/><br/>\r\n".getBytes());
+            writer.write("<br />\n".getBytes());
+            writer.write("Date: <input type=\"date\" name=\"date\"<br/><br/>\r\n".getBytes());
+            writer.write("<br />\n".getBytes());
+            writer.write("<input type=\"submit\" value=\"Submit\"/>\r\n".getBytes());
+            writer.write("</form>\r\n".getBytes());
+
+            writer.write("</body>\r\n".getBytes());
+            writer.write("</html>\r\n".getBytes());
         } catch (Exception e) {
             System.err.println(e);
         }
